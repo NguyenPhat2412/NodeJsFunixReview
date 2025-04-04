96e283b00c24479aa81952859784a8a908d1c53b
unknown <masterrio2412@gmail.com>
2025-04-04 23:27:48 +0700

    lab5

--- Lab5/app.js
+++ Lab5/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const ejs = require("ejs");
+// const ejs = require("ejs");
+
+// dùng pug
+app.set("view engine", "pug");
 // thiet lap thu muc views
 app.set("views", path.join(__dirname, "views"));
 
@@ -10,10 +13,14 @@ app.use(express.static("public"));
 
 // thiet lap cac duong dan
 app.get("/", (req, res) => {
-  res.render("shop.ejs");
+  res.render("shop");
 });
 app.get("/addproduct", (req, res) => {
-  res.render("addProduct.ejs");
+  res.render("addProduct");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
 app.listen(3000);
